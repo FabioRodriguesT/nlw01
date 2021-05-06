b1681e0d9de263bcd0f375356231fb83755228d4
Fábio Rodrigues <fabur13@gmail.com>
2021-05-06 15:28:08 -0300

    deploy

---
+++ precache-manifest.4e0a7d19863a9c793078be2fddb12fcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbe314962c14a7774e8f491d80bc205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a578cebd65713daa67ff",
+    "url": "/static/css/main.9754739a.chunk.css"
+  },
+  {
+    "revision": "5df69f39ded975d9be95",
+    "url": "/static/js/2.1c9f03fc.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/static/js/2.1c9f03fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a578cebd65713daa67ff",
+    "url": "/static/js/main.8ae7f2ea.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
