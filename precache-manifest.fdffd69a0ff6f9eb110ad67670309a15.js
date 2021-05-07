c9f3bd7bfdbf82542373d32756e8e15d715061ef
Fábio Rodrigues <fabur13@gmail.com>
2021-05-07 07:56:42 -0300

    Updates

---
+++ precache-manifest.fdffd69a0ff6f9eb110ad67670309a15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794a998d94bee20978abdb607154be70",
+    "url": "/nlw01/index.html"
+  },
+  {
+    "revision": "2cd5f33cb3854869b41d",
+    "url": "/nlw01/static/css/main.4db15188.chunk.css"
+  },
+  {
+    "revision": "5df69f39ded975d9be95",
+    "url": "/nlw01/static/js/2.1c9f03fc.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/nlw01/static/js/2.1c9f03fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd5f33cb3854869b41d",
+    "url": "/nlw01/static/js/main.df77a07e.chunk.js"
+  },
+  {
+    "revision": "b360a08e1661ed6de9f0",
+    "url": "/nlw01/static/js/runtime-main.d379b185.js"
+  },
+  {
+    "revision": "abd2ba69bb6bf8469130b703115dd2a2",
+    "url": "/nlw01/static/media/home-background.abd2ba69.svg"
+  },
+  {
+    "revision": "26434f1f8d4600c2289a91f27afe85a8",
+    "url": "/nlw01/static/media/logo.26434f1f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ecoleta/precache-manifest.42f26370f6fa04b6227cba4361d054a4.js"
+  "/nlw01/precache-manifest.fdffd69a0ff6f9eb110ad67670309a15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ecoleta/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nlw01/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
